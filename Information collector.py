65c796bd6e4b16ce245cb6807a34788b96a5017a
Kachiochemba <113605869+Kachiochemba@users.noreply.github.com>
2022-11-06 20:20:05 +0100

    Add files via upload

---
+++ Information collector.py
@@ -0,0 +1,7 @@
+#collection of input/information from the user
+Name = input("What is your name: ")
+Age = input("How old are you: ")
+Department = input("What deparment are you: ")
+
+#print out the total information received
+print("Your name is " + Name + "," + " you are " + Age + "years old." + " You are an " + Department + " student")
