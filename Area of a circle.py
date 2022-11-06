906703220d1ead5731390813fae5f826796e6ca7
Kachiochemba <113605869+Kachiochemba@users.noreply.github.com>
2022-11-06 20:31:27 +0100

    Add files via upload

---
+++ Area of a circle.py
@@ -0,0 +1,12 @@
+#Area of a circle = PI * Radius square
+#User inputs radius value
+radius = input("input the radius of the circle: ")
+
+#Pi has a constant value and is given
+Pi = 3.142
+
+#Store result in the variable "Area"
+Area = Pi * (int(radius)*int(radius))
+
+#print result
+print(Area)
